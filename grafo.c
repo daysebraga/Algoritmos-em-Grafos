51f1ecbf033087e9fc6be76b513d816caf278f16
Dayse Braga <daysebraga2803@gmail.com>
2024-12-12 20:38:26 -0300

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -10,7 +10,7 @@ struct vertice{
 typedef struct Lista lista;
 struct Lista{
     Vertice *head;
-};  
+};
 
 typedef struct grafo Grafo;
 struct grafo{
@@ -21,29 +21,30 @@ struct grafo{
    int Aresta;
 };
 
-Grafo* cria_grafo(int n){
+Grafo* cria_grafo(){
+    int n = 5;
     Grafo *GRAFO = malloc(sizeof (GRAFO));
 
     GRAFO->n_vertices = n;
-    GRAFO->n_arestas = 0;
+    GRAFO->n_arestas = 4;
     GRAFO->lista_adjacencias = malloc(n * sizeof(lista*));
 
     for(int i=0; i < n; i++){
         GRAFO->lista_adjacencias[i] = malloc(sizeof (lista));
         GRAFO->lista_adjacencias[i]->head = NULL;
-    }    
+    }
     return GRAFO;
 }
 void inicializa_grafo(Grafo *G){
     Grafo *GRAFO = malloc(sizeof (GRAFO));
-    
+
     if(G != NULL){
 
         for(int i=0; i < G->n_vertices; i++){
             for(int j=0; j <= G->n_arestas; j++){
                 GRAFO->lista_adjacencias[i] =  i;
                 insere_aresta(GRAFO,i,j);
-            }    
+            }
         }
     }
 }
@@ -58,26 +59,51 @@ Vertice *p;
         fprintf(saida, "-1\n");
     }
 }
+void imprime_matriz(Grafo *G){
+
+    for(int i=0; i < G->n_vertices; i++){
+        for(int j=0; j< G->n_arestas; j++){
+            printf("%d -> %d", G->lista_adjacencias[i][j]);
+        }
+    }
+}
 void insere_vertice(Grafo *G, Vertice *vinse){
-    Vertice *ve;
-    Vertice *atual;
-    Vertice *aux;
+
+    Vertice *atual = malloc(sizeof (Vertice));
 
     if(G != NULL){
-        for(int i=0; i <G->n_vertices; i++){
+        for(int i=0; i < G->n_vertices; i++)
             atual = G->lista_adjacencias[i]->head;
-
-            if(atual == NULL){
+             if(atual == NULL){
                 atual = vinse;
-            }
+
+            G->n_vertices++;
+        }
+    }
+}
+int buscar(Grafo * G, Vertice *v){
+ 
+    for(int i=0; i < G->n_vertices; i++){
+        for(int j=0; j < G->n_vertices; j++){
+            
+            if(G->lista_adjacencias[i] == v)
+                return v;
         }
     }
 }
 void insere_aresta(Grafo *G, int v1, int v2){
 
-    if(v1 > 0 && v2 >0)
-        G->lista_adjacencias[v1][v2] = G->Aresta;
+    for(int i=0; i < G->n_vertices; i++)
+        if(v1 > 0 && v2 >0)
+            G->lista_adjacencias = G->lista_adjacencias[v1][v2] = 1;
+
+    G->n_arestas++;
 }
+
+voi remove_vertice(Vertice *v, Grafo *G){
+
+}
+void remove_aresta()
 
 //void atualiza_adjacencia(Grafo G, Vertice V){
 
@@ -86,7 +112,7 @@ void insere_aresta(Grafo *G, int v1, int v2){
 int main(){
     Grafo *G;
 
-cria_grafo(n);
+cria_grafo();
 
     FILE *arquivo = fopen("saida.txt", "w");
 
